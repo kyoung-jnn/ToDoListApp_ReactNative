e550f36132fbc8b91df2e421b3ae853aff42c763
Rohkyoungjin <rudwls468@gmail.com>
2020-07-21 18:03:03 +0900

    splash 화면 추가 완료
    png 바꿔야함

--- App.js
+++ App.js
@@ -20,26 +20,30 @@ import {
 
 import ToDo from './ToDo.js';
 import uuid from 'uuid-random';
+import SplashScreen from 'react-native-splash-screen';
 
 const {height, width} = Dimensions.get('window'); // 현재 화면의 가로 세로 값 가져오기
 
 export default class App extends Component {
   state = {newToDo: '', toDos: {}, loadedToDos: false};
 
- componentDidMount = () =>{
-   this._loadToDos();
- }
-  
+  componentDidMount = () => {
+    this._loadToDos();
+  };
+
   render() {
-    const {newToDo, toDos,loadedToDos} = this.state;
-    if(!loadedToDos){
+    const {newToDo, toDos, loadedToDos} = this.state;
+    if (!loadedToDos) {
       console.log('data loading...');
+      setTimeout(() => {
+        SplashScreen.hide();
+      }, 1000);
     }
 
-   return (
+    return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content"></StatusBar>
-        <Text style={styles.title}>Todo List</Text>
+        <Text style={styles.title}>To Do List</Text>
         <View style={styles.card}>
           <TextInput
             style={styles.input}
@@ -48,15 +52,17 @@ export default class App extends Component {
             onChangeText={this._changeToDo}
             onSubmitEditing={this._addToDo}></TextInput>
           <ScrollView contentContainerStyle={styles.toDos}>
-            {Object.values(toDos).reverse().map((toDo) => (
-              <ToDo
-                key={toDo.id}
-                {...toDo}
-                deleteToDo={this._deleteToDo}
-                uncompleteToDo={this._uncompleteToDo}
-                completeToDo={this._completeToDo}
-                updateToDo={this._updateToDo}></ToDo>
-            ))}
+            {Object.values(toDos)
+              .reverse()
+              .map((toDo) => (
+                <ToDo
+                  key={toDo.id}
+                  {...toDo}
+                  deleteToDo={this._deleteToDo}
+                  uncompleteToDo={this._uncompleteToDo}
+                  completeToDo={this._completeToDo}
+                  updateToDo={this._updateToDo}></ToDo>
+              ))}
           </ScrollView>
         </View>
       </View>
@@ -70,20 +76,18 @@ export default class App extends Component {
       const parsedToDos = JSON.parse(toDos); // string -> obj
       console.log('불러오기');
       console.log(parsedToDos);
-      this.setState({loadedToDos: true, toDos : parsedToDos});
+      this.setState({loadedToDos: true, toDos: parsedToDos || {}});
     } catch (e) {
       console.log(e);
-
     }
   };
 
   _saveToDos = (newToDos) => {
     console.log('저장');
     console.log(JSON.stringify(newToDos));
-    try{
+    try {
       const saveToDos = AsyncStorage.setItem('toDos', JSON.stringify(newToDos)); // string 형식으로 저장
-
-    }catch(e){
+    } catch (e) {
       console.log(e);
     }
   };
@@ -192,14 +196,12 @@ export default class App extends Component {
       return {...newState};
     });
   };
-
- 
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1B9CFC',
+    backgroundColor: '#74b9ff',
     alignItems: 'center',
   },
   title: {
